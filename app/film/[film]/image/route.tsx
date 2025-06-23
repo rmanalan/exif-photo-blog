84aaa5e12abb26633e7d72d56024017eb15e7aac
Rich Manalang <rich@arcade.software>
2025-06-22 21:31:51 -0700

    fix: more encoding issues

--- app/film/[film]/image/route.tsx
+++ app/film/[film]/image/route.tsx
@@ -44,7 +44,7 @@ export async function GET(
 
   return new ImageResponse(
     <FilmImageResponse {...{
-      film,
+      film: encodeURIComponent(film),
       photos,
       width,
       height,
